5d7e1d8c3b7f760bc4ab3f9e29e803a2c6ea45c1
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-10 21:18:17 +0530

    Added:Logic for Displaying Welcome message of Binary Search tree

---
+++ src/com/java/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.java.binarySearchTree;
+
+public class BinarySearchTree {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Binary Search Tree program");
+	}
+
+}
