e5d55f08448756528ad62c436e2a53543125915a
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-11 23:11:03 +0530

    Added:Logic for Create The Binary search Tree and Checking the All are Added in the BST

--- src/com/java/binarySearchTree/BinarySearchTree.java
+++ src/com/java/binarySearchTree/BinarySearchTree.java
@@ -6,52 +6,57 @@ package com.java.binarySearchTree;
  */
 public class BinarySearchTree {
 
-	class Node
-	{
+	class Node {
 		int data;
-		Node left,right;
-		
-		Node(int data)
-		{
+		Node left, right;
+
+		Node(int data) {
 			this.data = data;
 		}
 	}
+
 	Node root;
+
 	public BinarySearchTree() {
 		root = null;
 	}
-	
-	void insert(int data)
-	{
-		root = insertRec(root,data);
+
+	void insert(int data) {
+		root = insertRec(root, data);
 	}
-	Node insertRec(Node root, int data)
-	{
-		if(root == null)
-		{
+
+	Node insertRec(Node root, int data) {
+		if (root == null) {
 			root = new Node(data);
 			return root;
 		}
-		
-		if(data < root.data)
+
+		if (data < root.data)
 			root.left = insertRec(root.left, data);
 		else
 			root.right = insertRec(root.right, data);
 		return root;
 	}
-	
-	void inOrder()
-	{
+
+	void inOrder() {
 		inOrderRec(root);
 	}
-	void inOrderRec(Node root)
-	{
-		if(root == null)
+
+	void inOrderRec(Node root) {
+		if (root == null)
 			return;
-		
+
 		inOrderRec(root.left);
-		System.out.print(root.data+" ");
+		System.out.print(root.data + " ");
 		inOrderRec(root.right);
+	}
+
+	// method to find the size i.e number of nodes in BST
+	public int size(Node root) {
+		if (root == null)
+			return 0;
+		else
+			return (size(root.left) + 1 + size(root.right));
 	}
 	/*
 	 * Program execution starts from main method
@@ -59,12 +64,25 @@ public class BinarySearchTree {
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
 		tree.insert(56);
-		tree.inOrder();
-		System.out.println();
 		tree.insert(30);
+		tree.insert(70);
+		tree.insert(22);
+		tree.insert(40);
+		tree.insert(11);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(60);
+		tree.insert(95);
+		tree.insert(65);
+		tree.insert(63);
+		// tree.insert(67);
 		tree.inOrder();
 		System.out.println();
-		tree.insert(70);
-		tree.inOrder();
-		
-	}}
+		int size = tree.size(tree.root);
+		if (size == 13)
+			System.out.println("All elements are added");
+		else
+			System.out.println("Missing some elements");
+
+	}
+}
